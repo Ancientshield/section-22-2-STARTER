96d0d0ae490c187d93ccf6479c0ec425abf1aafe
Ancientshield <jacklin2100@gmail.com>
2023-08-19 16:17:27 +0800

    Separation of Concerns Part 1

---
+++ src/components/EmailInput.vue
@@ -0,0 +1,25 @@
+<template>
+	<input
+		type="email"
+		class="form-control"
+		placeholder="E-mail"
+		:value="modelValue"
+		@input="update($event)"
+		:class="{
+			'is-valid': email.length >= 4 && email.length > 0,
+			'is-invalid': email.length && email.length > 0,
+		}"
+	/>
+</template>
+
+<script>
+	export default {
+		name: 'EmailInput',
+		props: ['email'],
+		methods: {
+			update() {
+				this.$emit('update:email', $event.target.value);
+			},
+		},
+	};
+</script>
